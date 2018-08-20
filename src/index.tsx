32b16df8f6db7a13ae97801a4023a9a3691c87fc
FleuryRomain <fleuryromain17@gmail.com>
2018-08-20 11:11:46 +0200

    install typescrit & parcel

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,13 @@
-import {h} from 'preact'
+import {h, render} from 'preact'
+
+let textareas = document.querySelectorAll('textarea.mdeditor') as NodeListOf<HTMLTextAreaElement>
+
+textareas.forEach(function(textarea){
+    let value = textarea.value
+    let name = textarea.getAttribute('name')
+    let div = document.createElement('div')
+    if(textarea.parentNode){
+        textarea.parentNode.replaceChild(div, textarea)
+        render(<div>Salut</div>,div)
+    }    
+})
